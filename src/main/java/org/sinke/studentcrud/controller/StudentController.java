ff810c3a2b576c0d1702ada6929ddf097f22b74e
sinke237 <enowsinke@gmail.com>
2024-03-15 15:19:43 +0100

    fxn(updateStudent): update student

--- src/main/java/org/sinke/studentcrud/controller/StudentController.java
+++ src/main/java/org/sinke/studentcrud/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -33,6 +34,25 @@ public class StudentController {
     @PostMapping("/student")
     public String saveStudent(@ModelAttribute("student") Student student){
         studentService.saveStudent(student);
+        return "redirect:/students";
+    }
+//  Updating Student
+    @GetMapping("/student/edit/{id}")
+    public String editStudent(@PathVariable Long id, Model model){
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit_student";
+    }
+
+    @PostMapping("/student/{id}")
+    public String updateStudent(@PathVariable Long id, @ModelAttribute("student") Student student){
+        Student existingStudent = studentService.getStudentById(id);
+        existingStudent.setId(id);
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+
+        studentService.updateStudent(existingStudent);
+
         return "redirect:/students";
     }
 }
