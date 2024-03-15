aaf929bb11252912c3593526da8e6368194ba1a9
sinke237 <enowsinke@gmail.com>
2024-03-15 12:27:30 +0100

    blueprint(Student), Student class, DI

--- src/main/java/org/sinke/studentcrud/entity/Student.java
+++ src/main/java/org/sinke/studentcrud/entity/Student.java
@@ -1,9 +1,18 @@
 package org.sinke.studentcrud.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "studentsCrud")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+    @Column(name = "email")
     private String email;
     public Student() {
     }
@@ -13,4 +22,35 @@ public class Student {
         this.email = email;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
