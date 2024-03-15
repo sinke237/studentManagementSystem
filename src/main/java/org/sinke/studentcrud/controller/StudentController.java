28f0d2ae473ca0cc390f19ba3366b8ee253180c4
sinke237 <enowsinke@gmail.com>
2024-03-15 15:56:11 +0100

    fxn(deleteStudent): delete student

--- src/main/java/org/sinke/studentcrud/controller/StudentController.java
+++ src/main/java/org/sinke/studentcrud/controller/StudentController.java
@@ -55,4 +55,12 @@ public class StudentController {
 
         return "redirect:/students";
     }
+
+//    delete student
+    @GetMapping("/student/{id}")
+    public String deleteStudent(@PathVariable Long id){
+        studentService.deleteStudent(id);
+
+        return "redirect:/students";
+    }
 }
--- src/main/java/org/sinke/studentcrud/service/impl/StudentServiceImpl.java
+++ src/main/java/org/sinke/studentcrud/service/impl/StudentServiceImpl.java
@@ -35,4 +35,10 @@ public class StudentServiceImpl implements StudentService {
         return studentRepository.save(student);
     }
 
+    @Override
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+
 }
